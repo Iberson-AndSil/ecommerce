a8ecf6d9e845ea93f050b2e0f44642cdfd130017
iber <iberson-silva@outlook.com>
2023-12-18 17:36:48 -0500

    feat: mapear de clases a tablas y relaciones JPA

--- src/main/java/com/projectsiber/ecommerce/model/Producto.java
+++ src/main/java/com/projectsiber/ecommerce/model/Producto.java
@@ -1,6 +1,17 @@
 package com.projectsiber.ecommerce.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "productos")
 public class Producto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nombre;
     private String descripcion;
@@ -8,12 +19,15 @@ public class Producto {
     private double precio;
     private int cantidad;
 
+    @ManyToOne
+    private Usuario usuario;
+
     public Producto() {
 
     }
 
     public Producto(Integer id, String nombre, String descripcion, String imagen,
-    double precio, int cantidad) {
+    double precio, int cantidad, Usuario usuario) {
         super();
         this.id = id;
         this.nombre = nombre;
@@ -21,6 +35,7 @@ public class Producto {
         this.imagen = imagen;
         this.precio = precio;
         this.cantidad = cantidad;
+        this.usuario = usuario;
     }
 
     public Integer getId() {
@@ -69,6 +84,14 @@ public class Producto {
 
     public void setCantidad(int cantidad) {
         this.cantidad = cantidad;   
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;   
     }
 
     @Override
--- src/main/java/com/projectsiber/ecommerce/model/Usuario.java
+++ src/main/java/com/projectsiber/ecommerce/model/Usuario.java
@@ -1,6 +1,20 @@
 package com.projectsiber.ecommerce.model;
 
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "usuarios")
 public class Usuario {
+    @Id
+    // id autoincrementable
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nombre;
     private String username;
@@ -10,12 +24,18 @@ public class Usuario {
     private String tipo;
     private String password;
 
-    public Usuario(){
+    @OneToMany(mappedBy = "usuario")
+    private List<Producto> productos;
+
+    @OneToMany(mappedBy = "usuario")
+    private List<Orden> ordenes;
+
+    public Usuario() {
 
     }
 
     public Usuario(Integer id, String nombre, String username, String email, 
-    String direccion, String telefono, String tipo, String password){
+    String direccion, String telefono, String tipo, String password) {
         super();
         this.id = id;
         this.nombre = nombre;
@@ -91,6 +111,14 @@ public class Usuario {
         this.password = password;   
     }
 
+    public List<Producto> getProductos() {
+        return productos;
+    }
+
+    public void setProductos(List<Producto> productos) {
+        this.productos = productos;
+    }
+
     @Override
     public String toString() {
         return "Usuario [id=" + id + ", nombre=" + nombre + ", username=" + username + 
@@ -98,5 +126,3 @@ public class Usuario {
         ", tipo=" + tipo + ", password=" + password + "]";
     }
 }
-
-
